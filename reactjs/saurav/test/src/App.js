c1608de85ca006ceafe85afe36be75e3b0a2071f
sauravsb99 <sauravsb99@gmail.com>
2019-10-28 01:26:10 +0530

    may i come innnnnnnnnnnnnn

--- reactjs/saurav/test/src/App.js
+++ reactjs/saurav/test/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './cgpu_2.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <h1> Sautaad</h1>
+      <header className="App-header"> May i come innnnnnnnnnnn
+      
+       <img src={logo} className='App-logo'></img>
+       </header>
     </div>
   );
 }
