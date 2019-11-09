d39c3b5a649a0b154d6282dfe1e53c9ecb69b36d
sachin-in1 <saching1998@cet.ac.in>
2019-11-09 14:04:16 +0530

    table added but data from each loop in different pages

--- sachin/cgpu++/routes/report.js
+++ sachin/cgpu++/routes/report.js
@@ -4,10 +4,20 @@ var mysql = require('mysql');
 var test='';
 var count=0;
 
+'use strict';
+var PdfTable = require('voilab-pdf-table');
 var PDFDocument, doc;
 // var fs = require('fs');
 PDFDocument = require('pdfkit');
 doc = new PDFDocument();
+table = new PdfTable(doc, {
+                bottomMargin: 30
+            });
+
+
+
+
+
 // doc.pipe(fs.createWriteStream('output.pdf'));
 // PDF Creation logic goes here
 // doc.end();
@@ -23,6 +33,65 @@ var connection = mysql.createConnection({
 	password : 'password',
 	database : 'nodelogin'
 });
+
+
+//table
+
+
+    table
+            // add some plugins (here, a 'fit-to-width' for a column)
+            .addPlugin(new (require('voilab-pdf-table/plugins/fitcolumn'))({
+                column: 'description'
+            }))
+            // set defaults to your columns
+            .setColumnsDefaults({
+                headerBorder: 'B',
+                align: 'right'
+            })
+            // add table columns
+            .addColumns([
+                {
+                    id: 'description',
+                    header: 'Product',
+                    align: 'left'
+                },
+                {
+                    id: 'quantity',
+                    header: 'Quantity',
+                    width: 50
+                },
+                {
+                    id: 'price',
+                    header: 'Price',
+                    width: 40
+                },
+                {
+                    id: 'total',
+                    header: 'Total',
+                    width: 70,
+                    renderer: function (tb, data) {
+                        return 'CHF ' + data.total;
+                    }
+                }
+            ])
+
+            // add events (here, we draw headers on each new page)
+         //   .onPageAdded(function (tb) {
+           //     tb.addHeader();
+            //});
+ 
+        // if no page already exists in your PDF, do not forget to add one
+      //  doc.addPage();
+ 
+        // draw content, by passing data to the addBody method
+//        table.addBody([
+  //          {description: `${count}`, quantity: 1, price: 20.10, total: 20.10}
+            
+    //    ]);
+ 
+ //       return doc;
+// table.addBody([{description: 'Product 2', quantity: 4, price: 4.00, total: 16.00},
+//            {description: 'Product 3', quantity: 2, price: 17.85, total: 35.70}]);
 
 
 router.get('/', function(req, res, next) {
@@ -49,7 +118,7 @@ res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
                                 .lineTo(doc.page.width-10,100)                            // draw a line
                                   .stroke(); 
                                 doc.moveDown(2);
-
+                    doc.fontSize(13);
     connection.connect(function(err) {
                 if (err) throw err;
                 console.log("Connected!");
@@ -66,23 +135,35 @@ res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
                 // console.log(json);
                 var i;
                 for (i=0;i<json.length;i++){
+                    if(count>0){doc.addPage();}
                     test=json[i].username;
+                    content=json[i].email;
                     count=count+1;
-                    // doc.text(json[i].username, {
-                    //     width: 410,
-                    //     align: 'left'
-                    // });
 
+//table
+
+
+                    table.addBody([{description: `${json[i].username}`, quantity: 1, price: 20.10, total: 20.10}
+                    ]);               
+                    table.onPageAdd(function (table, row, ev) {
+    // do something like
+                    table.doc.addPage();
+    // cancel event so the automatic page add is not triggered
+                        ev.cancel = true;
+                    });
                     //console.log(test);
 
 
                              //   doc.fontSize(13).text("Grievance Cell Report for the month"+"  "+monthInWords+" - "+dateToCheck.getFullYear(),{align:'left'})
                               //  doc.moveDown();
-                                doc.fontSize(13).text("Number of grievances recieved : "+test.length);
-                                doc.moveDown();
+//                                doc.fontSize(13).text("Letters : "+test.length);
+
+//WORKING PDF
+/*                                doc.moveDown();
                               //  doc.text("Number of grievances resolved : ");
 
-                                doc.text(`Number of grievances resolved : ${test}`);
+                                doc.fontSize(13).text(`User : ${test}`);
+                                doc.fontSize(13).text(`Email : ${content}`);
 
                                // for( i=0 ;i<json.length;i++){
                                 //console.log(content[i]);
@@ -101,101 +182,29 @@ res.setHeader('Content-disposition', 'attachment; filename="' + filename + '"');
                                 doc.moveTo(10,doc.page.height-18)                               // set the current point
                                 .lineTo(doc.page.width-10,doc.page.height-18)                            // draw a line
                                   .stroke();
-                                doc.fontSize(7).text("Grievance Cell,College Of Engineering Trivandrum",90,doc.page.height-15,{lineBreak:false})
+                                doc.fontSize(7).text("CGPU,College Of Engineering Trivandrum",70,doc.page.height-15,{lineBreak:false})
+                                doc.moveDown(0)
 
+*/ 
+//WORKING PDF 
 
-
+//table
 
 
                 }
-                    // console.log(test);
-                    // username="ss";
-doc.text(`no.of ${count}`);
+                
+doc.fontSize(13).text(`no.of ${count}`);
+
+
 doc.end();
                                 
-                  
-    
-    
-                });
-                                    
-    
-                                        
-    
-            });
+              
 
-            // console.log("content");
+    });
+                        
+});
 
-             var content = test;
-    // var publish_date = datetime;
-    // var author_name  = "CGPU++";
-    // var link         = "www.cet.ac.in";
-    
-    //   doc.fontSize(15)
-    //      .fillColor('blue')
-    //      .text('Read Full Article', 100, 100)
-    //      .link(100, 100, 160, 27, link);
-    //   doc.moveDown()
-    //      .fillColor('red')
-    //      .text("Author: "+author_name);
-      
-    //   doc.moveDown()
-    //      .fillColor('black')
-    //      .fontSize(15)
-    //      .text(content, {
-    //        align: 'justify',
-    //        indent: 30,
-    //        height: 300,
-    //        ellipsis: true
-    //      });
-//test="hello";
+
     console.log(test);
-//for( i=0 ;i<content.length;i++){
-//      console.log( content[i]);
-//      doc.text(content[3],250,5000);
-//    }
-
-        //doc.text(content,250,5000);
-      
-
-      
-//doc.on('pageAdded', () => doc.text("Page Title"));
-    
-    // if( option = 1){
-    
-    //     connection.connect(function(err) {
-    //         if (err) throw err;
-    //         console.log("Connected!");
-    //         var sql = "SELECT username FROM  accounts ";
-    //         connection.query(sql,function (err, result) {
-    //           if (err) throw err;
-    //         //   console.log(result);
-    //           var string=JSON.stringify(result);
-    //     				// console.log('>> string: ', string );
-    //           var json =  JSON.parse(string);
-              
-    //         //   var username=json.username;
-    //         // console.log(json[70]);
-    //         var i;
-    //         for (i=0;i<json.length;i++){
-
-    //             doc.text(json[i].username, {
-    //                 width: 410,
-    //                 align: 'left'
-    //             });
-    //             // console.log(json[i].username);
-    //         }
-
-    //             // username="ss";
-
-              
-
-
-    //         });
-
-
-
-    //     });
-    // }
-        
 });
 module.exports = router;
